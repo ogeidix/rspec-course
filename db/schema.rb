ae964d59044c2fbab1b71a66ce11f87b3fa07997
ogeidix <diegogiorgini@gmail.com>
2011-11-19 13:41:23 +0100

    rails g migration add_nickname_to_users nickname:string

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111119102249) do
+ActiveRecord::Schema.define(:version => 20111119123924) do
 
   create_table "users", :force => true do |t|
     t.string   "name",       :default => ""
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20111119102249) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "nickname"
   end
 
 end
