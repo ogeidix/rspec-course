ed72650eafea0d04f3e8d47a7b4d0bbf8dfb19fe
ogeidix <diegogiorgini@gmail.com>
2011-11-19 11:58:02 +0100

    First spec implemented

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 describe User do
 
-  pending "require a unique email to be valid"
+  it "require a unique email to be valid" do
+    user1 = User.create(:email => 'one@rr.com')
+    user2 = User.create(:email => 'one@rr.com')
+    user2.should_not be_valid
+    user2.should have(1).error_on(:email)
+  end
 
 end
