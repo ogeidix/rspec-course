655d022c251a1e08877dbcb19f2c9d38d62ecc9a
ogeidix <diegogiorgini@gmail.com>
2011-11-19 10:57:30 +0100

    rails new rspec-course --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RspecCourse::Application.config.secret_token = '9866f99e9199c704aa891bdaa822ea869c85c939d91a6267de7328098efa0d044b36d56d61c11c56eb9ec185c63129933d198706602081d0577c6cf84d556e3c'
